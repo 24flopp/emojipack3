57b53c4f991b6c56b0fa56f5baa5b43b71a84eb6
24flopp <84596062+24flopp@users.noreply.github.com>
2023-01-31 15:35:20 +0100

    Update repoData.js

--- repoData.js
+++ repoData.js
@@ -1,14 +1,14 @@
 //Add Name of your Repo here
-exports.name = 'Example Repo';
+exports.name = "Floppa's Emojis";
 //Add Name and Type of Image Icon you want to use here (Must be PNG)
 exports.icon = 'icon.png';
 //Add the folder name where all your emotes are saved
 exports.pathName = 'emotes';
 //Add your name here, or remove the line if you don't need your name displayed under the repo's title in our clients
-exports.author = 'Joe';
+exports.author = '24Flopp#0546';
 //Add a Description here, useful for SEO and Web Crawlers, or sharing to social websites. Remove the line if you don't care about Sharing.
-exports.description = 'Example Description';
+exports.description = 'floppa';
 //Add Keywords here, useful for SEO and Web Crawlers. Remove the line if you don't care about SEO/Web Crawlers.
-exports.keywords = 'Nitroless, nitroless, iOS, macOS, Android, Windows, Emotes, Discord, discord, Nitro, nitro';
+exports.keywords = 'Nitroless, nitroless, iOS, macOS, Android, Windows, Emotes, Discord, discord, Nitro, nitro, floppa, caracal, caracal cat, no nitro, emojis, discord emojis, slack emojis, Slack, slack, emotes, discord emotes, slack emotes';
 //Add the folder name where all your stickers are saved
 exports.stickerPath = 'stickers'
